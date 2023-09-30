ac4b25fef74bc01271999435a4c99680638c8f28
Jose Felipe Alexandre <felipealexandre178@gmail.com>
2023-09-30 16:35:05 -0300

    feat: Add Funcionario

---
+++ src/models/funcionarioModel.ts
@@ -0,0 +1,39 @@
+export interface Funcionario{
+    Nome: string;
+    email: string;
+    telefone: number;
+    funcionarioId: number;
+}
+
+const funcionarios: Funcionario[] = [];
+
+export function getAllFuncionarios(): Funcionario[]{
+    return funcionarios;
+}
+
+export function addFuncionario(funcionario: Funcionario): Funcionario{
+    funcionarios.push(funcionario);
+    return funcionario;
+}
+
+export function getFuncionarioById(id: number): Funcionario | undefined {
+    return funcionarios.find((funci) => funci.funcionarioId === id);
+}
+
+export function updateFuncionario(id: number, updatedFuncionario: Funcionario): Funcionario | undefined {
+    const index = funcionarios.findIndex((funci) => funci.funcionarioId === id);
+    if(index !== -1){
+        funcionarios[index] = {...updatedFuncionario, funcionarioId: id};
+        return funcionarios[index];
+    }
+    return undefined;
+}
+
+export function deleteFuncionario(id: number): Funcionario | undefined {
+    const index = funcionarios.findIndex((funci) => funci.funcionarioId === id);
+    if (index !== -1) {
+        const deletedFuncionario = funcionarios.splice(index, 1)[0];
+        return deletedFuncionario;
+    }
+    return undefined;
+}
