650930d86f0fc787ec30828c837fc2db19c9ceae
EzioKenway1 <nathan48silva@gmail.com>
2023-10-02 23:12:07 -0300

    test:  CRUD route of clothes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import route from './routes/roupaRoute';
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use('/api/roupas', route);
 
 
 app.listen(port, () => {
